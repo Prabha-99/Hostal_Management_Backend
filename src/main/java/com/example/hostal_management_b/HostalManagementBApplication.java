9263e7593c844d4a698ec94399382409d4db5056
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-09-30 19:35:21 +0530

    Initial Commit

---
+++ src/main/java/com/example/hostal_management_b/HostalManagementBApplication.java
@@ -0,0 +1,13 @@
+package com.example.hostal_management_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostalManagementBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HostalManagementBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hostal_management_b/HostalManagementBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hostal_management_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostalManagementBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
