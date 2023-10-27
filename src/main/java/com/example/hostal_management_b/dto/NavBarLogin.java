d29879a9f8ed6202e9bd6c7bcb196cb6cf448643
KBCHANUKAJ <chanukakbc@gmail.com>
2023-10-27 16:17:13 +0530

    Get Current user

---
+++ src/main/java/com/example/hostal_management_b/dto/NavBarLogin.java
@@ -0,0 +1,19 @@
+package com.example.hostal_management_b.dto;
+
+public class NavBarLogin {
+    private String firstname;
+
+
+    public NavBarLogin(String firstname) {
+        this.firstname = firstname;
+
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+}
