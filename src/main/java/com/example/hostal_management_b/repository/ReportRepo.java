fcc998f36f136cacab8530fd74e8bba894883337
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2023-11-03 15:48:20 +0530

    update

--- src/main/java/com/example/hostal_management_b/repository/ReportRepo.java
+++ src/main/java/com/example/hostal_management_b/repository/ReportRepo.java
@@ -4,8 +4,10 @@ import com.example.hostal_management_b.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReportRepo extends JpaRepository<Report, Long> {
 
-
+    List<Report> findAll();
 }
--- src/main/java/com/example/hostal_management_b/service/ReportService.java
+++ src/main/java/com/example/hostal_management_b/service/ReportService.java
@@ -1,6 +1,70 @@
 package com.example.hostal_management_b.service;
 
+import com.example.hostal_management_b.repository.ReportRepo;
+import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class ReportService {
+    private final JdbcTemplate jdbcTemplate;
+    private final ReportRepo reportRepo;
+
+    //Local variable to Store current Data.
+    LocalDate currentDate = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    String dateCreated = currentDate.format(formatter);
 
 
+    @Scheduled(cron = "0 0 21 * * ?")
+    public String exportDailyReport() throws FileNotFoundException, JRException {
+        String reportPath = "D:\\Generated_Reports";
+
+        List<Product> products=productRepo.findAll();//Retrieving all User Data into a List
+
+        //Loading the .jrxml file and Compiling it
+        File file= ResourceUtils.getFile("classpath:Products.jrxml");
+        JasperReport jasperReport= JasperCompileManager.compileReport(file.getAbsolutePath());
+
+        //Mapping List Data into the Report
+        JRBeanCollectionDataSource source=new JRBeanCollectionDataSource(products);
+        Map<String,Object> parameters=new HashMap<>();
+        parameters.put("Created by","Faculty of Technology");
+
+        // Saving the report file to the database
+        String sql = "INSERT INTO reports (report_name, path, date) VALUES (?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, "Products"+dateCreated+".pdf");
+            ps.setString(2,reportPath+".pdf");
+            ps.setTimestamp(3, new Timestamp(System.currentTimeMillis())); // set the current date and time
+            return ps;
+        }, keyHolder);
+
+        //Printing the Report
+        JasperPrint print= JasperFillManager.fillReport(jasperReport,parameters,source);
+        JasperExportManager.exportReportToPdfFile(print,reportPath+"\\Products.pdf");
+
+
+        return "Report generated Successfully at : "+reportPath;
+    }
 }
