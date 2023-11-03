88541f10a22546afaeea8a8e94512c8ed549ae6f
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2023-11-03 15:32:47 +0530

    Created a UserDto class for the purpose of getting users firstname, lastname and reg_no

--- src/main/java/com/example/hostal_management_b/dto/UserDto.java
+++ src/main/java/com/example/hostal_management_b/dto/UserDto.java
@@ -1,2 +1,14 @@
-package com.example.hostal_management_b.dto;public class UserDto {
+package com.example.hostal_management_b.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {   //For getting firstname and Lastname for Viewing purposes of Angular Frontend
+    private String firstname;
+    private String lastname;
+    private String reg_no;
 }
